58000797b8373c5f8645c7c1a8bbbd8017298edc
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-10-01 13:39:16 +0200

    task 1 done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+let element1 = CreateNewElement("hot","seven","bongus")
+console.log(element1)
+
+function CreateNewElement(classatt,idatt,text)
+{
+    let NewElement = document.createElement("li")
+    NewElement.setAttribute("id",idatt)
+    NewElement.className = classatt
+    let NewTextElement = document.createTextNode(text)
+    NewElement.append(NewTextElement)
+    return NewElement
+}
