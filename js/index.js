89d8cbbbd5e249822cb5ed7c99909daa55606f0a
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-10-01 14:22:03 +0200

    other tasks done oops

--- js/index.js
+++ js/index.js
@@ -5,6 +5,9 @@ let element1 = createNewElement("unhealthy","7","Pizza")
 console.log(element1)
 
 appendToList("listUnhealthy",element1)
+changeClass(7,"healthy")
+deleteFromList("listUnhealthy",7)
+changeText(2,"Soda")
 
 function createNewElement(classatt,idatt,text)
 {
@@ -20,4 +23,27 @@ function appendToList(listid, listelement)
 {
     let listToAppend = document.getElementById(listid)
     listToAppend.append(listelement)
+}
+
+function changeClass(elementid, attributetochange)
+{
+    let element = document.getElementById(elementid)
+    if (element.className === null) element.className = attributetochange
+    else element.setAttribute("class",attributetochange)
+}
+
+function deleteFromList(listid, elementid)
+{
+    let list = document.getElementById(listid)
+    let element = document.getElementById(elementid)
+    if(element.parentElement === list)
+        {
+            list.removeChild(element)
+        }
+}
+
+function changeText(elementid, text)
+{
+    let element = document.getElementById(elementid)
+    element.textContent = text
 }
